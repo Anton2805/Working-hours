de1aae4e8276c523bad2fff6409a9868ef3ace2e
Anton <maas@yandex.ru>
2018-06-04 21:26:22 +0500

    Новая версия

--- WorkingHours/WorkingHours/Model/ModelDay.cs
+++ WorkingHours/WorkingHours/Model/ModelDay.cs
@@ -10,6 +10,14 @@ namespace WorkingHours.Model
     {
         public DateTime Date { get; set; }
         public string Day { get; set; }
-
+        public string TimesDay { get; set; }
+        public string TimeDebt { get; set; }
+        public string leaveDebt { get; set; }
+        public string comingDebt { get; set; }
+        public string timeDebt { get; set; }
+        public string leaveWorkingOff { get; set; }
+        public string comingWorkingOff { get; set; }
+        public string timeWorkingOff { get; set; }
+        public string balanceValue { get; set; }        
     }
 }
