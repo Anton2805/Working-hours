59cd0183cf8e16adb27d6c729fad17eb2abf4442
Anton <maas@yandex.ru>
2018-06-17 21:17:37 +0500

    Подготовка к модели

--- WorkingHours/WorkingHours/Model/Model.cs
+++ WorkingHours/WorkingHours/Model/Model.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 namespace WorkingHours.Model
 {
-    class ModelDay
+    class Model
     {
+        /// <summary>
+        /// Дата долга или отработки
+        /// </summary>
         public DateTime Date { get; set; } // Дата долга или отработки
         public string Day { get; set; } // День недели долга или отработки
-        public string TimesDay { get; set; } // Время суток, в течение которого произршли либо долг либо отрботка
+        public string TimesDay { get; set; } // Время суток, в течение которого произошли либо долг, либо отрботка
         public string timeDebt { get; set; } // Продолжительность долга
         public string leaveDebt { get; set; } // Уход в рабочее время
         public string comingDebt { get; set; } // Приход в рабочее время       
         public string comingWorkingOff { get; set; } // Приход на отработку
         public string leaveWorkingOff { get; set; } // Уход с отработки
         public string timeWorkingOff { get; set; } // Время отработки
-        public string balanceValue { get; set; } // Дебет времени на текущий момент
+        public string totalTime { get; set; } // Дебет времени на текущий момент
     }
 }
